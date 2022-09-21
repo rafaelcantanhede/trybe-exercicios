65279a23e90a004d43a86d8e591fcf242bc1b85f
Rafael Cantanhede <rafaelsena346@gmail.com>
2022-09-21 17:01:53 -0300

    adiciono ifelse na pasta de js

---
+++ exercicio ifelse.js
@@ -0,0 +1,17 @@
+let nota = 2
+
+if (nota >= 80) {
+    console.log("Parabéns, vc foi aprovado o seu fudido")
+}
+
+else if (nota <= 80) {
+    console.log("Voce se fudeu")
+}
+
+else if (nota < 60){ 
+    "voce ta muito fudido"
+}
+
+else {
+    "absurdamente se fudeu"
+}
