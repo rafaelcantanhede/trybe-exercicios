2f4fa9ab93ab157e2e7f5790d9ef86db5f4be1f9
Rafael Cantanhede <rafaelsena346@gmail.com>
2022-09-22 19:18:16 -0300

    adiciona exercicios For

---
+++ fundamentos/Javascript/Dia2-Js-Array-e-LoopFor/For1.js
@@ -0,0 +1,5 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index++) {
+    let mensagem = "Compre imediatamente isso aqui:" + groceryList[index];
+    console.log(mensagem)
+}
